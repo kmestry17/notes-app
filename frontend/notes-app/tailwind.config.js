a3e861283b14839c26c42c1fda934deb12980312
Kunal Mestry <kmestry17@gmail.com>
2024-06-27 16:58:50 -0500

    login page design

--- frontend/notes-app/tailwind.config.js
+++ frontend/notes-app/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // colors used in the project
+      colors: {
+        primary: "#2B85FF",
+        secondary: "#EF863E",
+      },
+    },
   },
   plugins: [],
 };
