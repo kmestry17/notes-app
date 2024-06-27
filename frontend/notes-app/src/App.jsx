7a9860f98f8fb9e8615dd5ef88ef9ac6510cd72f
Kunal Mestry <kmestry17@gmail.com>
2024-06-27 16:30:12 -0500

    routing methods and tailwind css

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+
+const routes = (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  </Router>
+);
 
 const App = () => {
-  return (
-    <div>
-      <Home />
-    </div>
-  );
+  return <div>{routes}</div>;
 };
 
 export default App;
