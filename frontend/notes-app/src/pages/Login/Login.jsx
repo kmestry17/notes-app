f90e02e2d36eb0b7a2f8e1eec739988cd4c91fe2
Kunal Mestry <kmestry17@gmail.com>
2024-06-27 16:39:19 -0500

    navbar initial

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 };
 
